989408cf7d054d9fe704f74f591404ce298affc7
Edward Atkin <edward.atkin@live.com>
2024-01-06 14:53:58 +0000

    Add scrolling ticker text

--- david_curses_interface.py
+++ david_curses_interface.py
@@ -40,6 +40,32 @@ def get_david_logo_ascii():
     david_ascii = su.image_to_ascii(david_logo, url=False)
     return david_ascii
 
+def print_ticker(stdscr, text, ticker_x):
+    """Prints the ticker text with scrolling"""
+    width = curses.COLS
+    ticker_spacing = round(width * 0.2)
+    # Print the ticker with ticker_x being the offset by exploding it into a list
+    ticker = list(text)
+    # Add enough repeats of the ticker text to fill the screen
+    while len(ticker) < width * 2:
+        ticker = ticker + list(" " * ticker_spacing) + ticker
+
+    # Slice the ticker text to fit the screen
+    ticker = ticker[ticker_x:ticker_x + width - 1]
+
+    # Join the ticker back into a string
+    ticker = "".join(ticker)
+    # Print the ticker with a colour pair
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    YELLOW_BLACK = curses.color_pair(1)
+    stdscr.addstr(ticker, curses.A_ITALIC | curses.A_BLINK | YELLOW_BLACK)
+
+    # Wrap ticker_x
+    if ticker_x >= len(text) + ticker_spacing:
+        ticker_x = 0
+
+    return ticker_x
+
 def main(stdscr):
     """Main function"""
     stdscr = curses.initscr()
@@ -66,11 +92,34 @@ def main(stdscr):
         stdscr.refresh()
         exit(1)
 
-    # Print welcome message
-    stdscr.addstr(f"Welcome to David Social version {version}!")
-    stdscr.refresh()
-    sleep(2)
-    pass
+    # Get the ticker text
+    ticker_text = david_api.query_api("get-ticker-text")
+    # Log the ticker text
+    logging.info(f"Ticker text: {ticker_text}")
+    if ticker_text is None:
+        ticker_text = "Ticker unavailable"
+    ticker_x = 0
+    # Scrolling ticker text causes some glitching which is annoying
+    ticker_update_rate = 0.2
+    t = datetime.now()
+
+    # Main loop
+    while True:
+        # Print welcome message
+        stdscr.clear()
+        curses.curs_set(0)
+        # Print the ticker text and handle scrolling
+        ticker_x = print_ticker(stdscr, ticker_text, ticker_x)
+        dt = datetime.now() - t
+        if dt.total_seconds() > ticker_update_rate:
+            t = datetime.now()
+            ticker_x += 1
+
+        stdscr.addstr("\n\n")
+        stdscr.addstr(f"Welcome to David Social version {version}!")
+
+        stdscr.refresh()
+        curses.doupdate()
 
 LOGFILE = logging_init()
 
